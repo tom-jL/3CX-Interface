b48f6306b4f1c3d1027430e5a9058e805a94b4fb
thomasleumann <thomas.leumann@my.jcu.edu.au>
2020-07-02 00:40:38 +1000

    update table association

--- app/Contact.php
+++ app/Contact.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'contacts';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
